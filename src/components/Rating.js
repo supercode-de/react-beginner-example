9f9a34238f576c4bae8f8e6cce4526b0e268e24b
David Bigus <david.bigus@gmail.com>
2021-03-24 17:48:28 +0100

    init

---
+++ src/components/Rating.js
@@ -0,0 +1,34 @@
+import StarFull from './assets/star_full.svg'
+import StarHalf from './assets/star_half.svg'
+import StarEmpty from './assets/star_empty.svg'
+
+
+const Rating = (props) => {
+
+    // comment
+
+    const rating = parseInt(props.rating);
+    
+    const stars = [];
+
+    for (let i = 0; i < 10; i++) {
+
+        let star = StarFull;
+
+        if (i > rating - 1) {
+            star = StarEmpty;
+        }
+
+        stars.push(<img key={i} src={star} />)
+    }
+
+    return ( 
+
+        <div className="rating">
+            { stars.map(item => item )}
+        </div>
+
+    );
+}
+ 
+export default Rating;
